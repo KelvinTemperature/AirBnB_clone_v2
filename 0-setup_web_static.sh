6bc75935b926b9b8086168bf1df34f84f6f65d8e
Kelvin <koolkt10@gmail.com>
2024-04-06 17:00:41 -0700

    fixing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,6 +12,6 @@ sudo touch /data/web_static/releases/test/index.html
 sudo echo "Hello World!" | sudo tee  /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
